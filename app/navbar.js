fbb852fafed506f6e0686d2638b11dd07d97ab48
Nicolis Yuvienco <nickyuvienco@gmail.com>
2021-01-07 03:47:01 -0800

    Add responsive/sticky nav bar on scroll

--- app/navbar.js
+++ app/navbar.js
@@ -1,10 +1,18 @@
 // variables
 const hamBtn = document.querySelector('.hamburger');
 const hamMenu = document.querySelector('#mobile-nav-links');
+const desktopNavBar = document.querySelector('#desktop-nav');
 
 export const navBar = () => {
   hamBtn.addEventListener('click', () => {
     hamBtn.classList.toggle('active');
     hamMenu.classList.toggle('active');
+  });
+
+  document.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition > 250) {
+      desktopNavBar.style.backgroundColor = '#023047';
+    } else desktopNavBar.style.backgroundColor = 'transparent';
   });
 };
